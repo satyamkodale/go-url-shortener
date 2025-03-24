ebbf1a4bcdd0a1c44e3c17e00cad2017a7d20469
satyamkodale <satyamkodale@gmail.com>
2025-03-24 20:49:46 +0530

    added home handler

--- main.go
+++ main.go
@@ -5,8 +5,13 @@ import (
 	"net/http"
 )
 
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome to URL Shortner")
+}
+
 func main() {
 	fmt.Println("<<<<<<<< Starting Server At 1911 >>>>>>>>")
+	http.HandleFunc("/", homeHandler)
 	error := http.ListenAndServe(":1911", nil)
 	if error != nil {
 		fmt.Println("Error for starting the server")
